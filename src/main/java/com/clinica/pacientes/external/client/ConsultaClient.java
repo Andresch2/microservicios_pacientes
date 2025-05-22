d7675b7e3668211ff3005c5d2d756da86a8c73a7
nelson.chaves@camousucc.edu.co <nelson.chaves@campusucc.edu.co>
2025-05-22 14:34:16 -0500

    corrigiendo url

--- src/main/java/com/clinica/pacientes/external/client/ConsultaClient.java
+++ src/main/java/com/clinica/pacientes/external/client/ConsultaClient.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import com.clinica.pacientes.external.dto.ConsultaDTO;
 
-@FeignClient(name = "consultas", url = "https://final-project-oop-production.up.railway.app/")
+@FeignClient(name = "consultas", url = "https://final-project-oop-production.up.railway.app")
 public interface ConsultaClient {
     @GetMapping("/consulta/paciente/{id}")
     List<ConsultaDTO> getConsultasPorPaciente(@PathVariable("id") Long pacienteId);
